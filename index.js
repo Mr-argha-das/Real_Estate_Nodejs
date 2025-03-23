8b374bb8ed7b81316ead983af301720d5d7cd4e6
Mr Das <81897206+Mr-argha-das@users.noreply.github.com>
2025-03-23 14:18:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ app.get("/health", (req, res) => {
 app.use("/api", Router);
 app.use("/uploads", express.static("uploads"));
 
-app.listen(port, "localhost", () => {
+app.listen(8080, "localhost", () => {
   console.log(`Server Run Port: ${port}`);
 });
