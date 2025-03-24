f706e943e355c492feb555cd433256cf2b808e2d
Abhay <abhaykoli214@gmail.com>
2025-03-24 13:04:37 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const MONGO_URL =
-  "mongodb+srv://avbigbuddy:nZ4ATPTwJjzYnm20@cluster0.wplpkxz.mongodb.net/RealEstateDataBase";
+  "mongodb+srv://avbigbuddy:nZ4ATPTwJjzYnm20@cluster0.wplpkxz.mongodb.net/RealEstateDataBase2";
 const database = () => {
   mongoose
     .connect(`${MONGO_URL}`)
